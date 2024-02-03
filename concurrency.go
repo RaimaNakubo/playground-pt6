e5f82d5a53983de5d3b12c90535a998bc08d2bbc
Nikita Belenkov <bna.gorzar@gmail.com>
2024-02-03 12:16:23 +0300

    A tour of Go n41: sync.Mutex

--- concurrency.go
+++ concurrency.go
@@ -1,117 +1,46 @@
-/*
-Exercise: Equivalent Binary Trees
-1. Implement the Walk function.
-
-2. Test the Walk function.
-
-The function tree.New(k) constructs a randomly-structured (but always sorted) binary tree holding the values k, 2k, 3k, ..., 10k.
-
-Create a new channel ch and kick off the walker:
-
-go Walk(tree.New(1), ch)
-Then read and print 10 values from the channel. It should be the numbers 1, 2, 3, ..., 10.
-
-3. Implement the Same function using Walk to determine whether t1 and t2 store the same values.
-
-4. Test the Same function.
-
-Same(tree.New(1), tree.New(1)) should return true, and Same(tree.New(1), tree.New(2)) should return false.
-
-Doc for Tree package - https://pkg.go.dev/golang.org/x/tour/tree#Tree
-*/
-
 package main
 
 import (
 	"fmt"
-
-	"golang.org/x/tour/tree"
+	"sync"
+	"time"
 )
 
+// SafeCounter is used to count some strings and safe to use concurrently
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
+}
+
 func main() {
+	//c is an instance of our SafeCounter initialized with unlocked mutex(zero value) for mutex-lock and an empty map for value
+	c := SafeCounter{v: make(map[string]int)}
 
-	//testing Walk() in another goroutine
-	ch := make(chan int)
-	go Walk(tree.New(1), ch)
-	for i := range ch {
-		fmt.Println(i)
-	}
-	fmt.Println()
+	for i := 0; i < 1000; i++ { //for a thousand times
+		go c.Inc("somekey") //incrementing counter for given key via separate goroutines
+	} //so a thousand sub-goroutines will try to access value in counter
 
-	ch = make(chan int) //have to create a new channel bc Walk() closes it
-	go Walk(tree.New(3), ch)
-	for i := range ch {
-		fmt.Println(i)
-	}
-	fmt.Println()
+	//instantly after launching all sub-goroutines checking for a value in counter
+	fmt.Println(c.Value("somekey")) //counter != 1000 bc some sub-goroutines cannot bypass locked mutex to increment counter in that short period of time
 
-	//testing Same() with not necessarely identical trees, but with the same values for nodes total;
-	//different arrangement, but same values
-	k := 3                                // k is a multiplier for values of a tree
-	res := Same(tree.New(k), tree.New(k)) //exp r true
-	fmt.Println(res)
-
-	//different arrangement, different values
-	res = Same(tree.New(k), tree.New(1)) //exp r false
-	fmt.Println(res)
+	//pausing main goroutine to wait for all sub-goroutines to complete their calls
+	time.Sleep(time.Second)
+	fmt.Println(c.Value("somekey")) //counter == 1000
 }
 
-// function Walk() walks the tree t sending all values from the tree to the channel ch and closes ch.
-func Walk(t *tree.Tree, ch chan int) {
-	defer close(ch) //before Walk() returns close the channel
+// method Inc() safely per-goroutine increments counter from reciever for the key given in argument
+func (c *SafeCounter) Inc(key string) {
+	//locking mutex for map c.v so only one goroutine can access the c.v map at a time
+	c.mu.Lock()
 
-	var nodeSearch func(t *tree.Tree) //defining a recursive function that searches for existing nodes in the tree
-
-	//nodeSearch acts like a container for all the recursive calls
-	nodeSearch = func(t *tree.Tree) {
-		//if there is no node then search stops
-		if t == nil {
-			//fmt.Println("There is no node")
-			return
-		} /*else {
-			fmt.Printf("There is a node: %v\n", t.Value)
-		}
-
-		if t.Left != nil {
-			fmt.Printf("There is a node on the left: %v\n", t.Left.Value)
-		} else {
-			fmt.Println("There is no node on the left")
-		}
-		if t.Right != nil {
-			fmt.Printf("There is a node on the right: %v\n", t.Right.Value)
-		} else {
-			fmt.Println("There is no node on the right")
-		}*/
-
-		//if there is a node then
-		nodeSearch(t.Left) //search for node on the left
-		ch <- t.Value      //send node's value to the channel
-		//fmt.Printf("a node have been sent to the channel: %v\n", t.Value)
-		nodeSearch(t.Right) //search for node on the right
-	}
-
-	nodeSearch(t) //calling recursive function to search for nodes in tree t
-
+	c.v[key]++    //incrementing counter for given key
+	c.mu.Unlock() //unlocking mutex
 }
 
-// function Same() determines whether the trees t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool {
-	//channels ch1 and ch2 are recievers for data from binary trees
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+// method Value() safely per-goroutine returns the current value of the counter from reciever for a given key from argument
+func (c *SafeCounter) Value(key string) int {
+	c.mu.Lock()         //locking mutex for c.v map
+	defer c.mu.Unlock() //mutex will unlock automaticly once this method returns
 
-	//calling Walk() in separate goroutines which simultaneously sends data from trees to channels
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
-
-	for v1 := range ch1 { //while first channel is open
-		if v1 != <-ch2 { //compare all recieved values one-by-one (operator awaits value from ch2)
-			// if ch2 is closed before ch1 closes then panic occurs; add channel's status comparison to handle this case if trees have different length
-			return false //if any pair doesn't match then trees are not the same
-		}
-	}
-
-	//if all recieved values from ch1 matches corresponding values recieved from ch2,
-	//then trees are the same
-	return true
+	return c.v[key] //returning counter for a given key
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module playground-pt6
 
 go 1.21.6
-
-require golang.org/x/tour v0.1.0
